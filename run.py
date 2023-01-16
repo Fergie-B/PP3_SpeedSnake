ad5b367b983f0c3fe7a5d341e205fe56784f44fe
Barry Ferguson <barry@revolvecreative.ie>
2023-01-16 17:15:59 +0000

    Wired up APIs and confirmed access to Google Sheet, to access and store player scores

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+
+# Scope Code to call Google Drive
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('speed_snake_scores')
+
+easy = SHEET.worksheet('easy')
+medium = SHEET.worksheet('medium')
+hard = SHEET.worksheet('hard')
+
+data = easy.get_all_values()
+
+print(data)
