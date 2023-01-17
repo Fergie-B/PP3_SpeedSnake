e1300f30450793e8ce56ef569f8eaebe7d58a76c
Barry Ferguson <barry@revolvecreative.ie>
2023-01-17 19:28:34 +0000

    Add Snake Head and Food position on game board, and set initial score

--- game.py
+++ game.py
@@ -14,3 +14,19 @@ import colorama
 from colorama import Fore
 colorama.init(autoreset=True)
 
+# set up screen
+setup = curses.initscr()
+h, w = setup.getmaxyx()
+win = curses.newwin(h, w, 0, 0)
+
+win.keypad(1)
+curses.curs_set(0)
+
+#Set Snake and Food Start positions
+snake_head = [10,10]
+food_position = [15,15]
+score = 0
+
+print(snake_head)
+print(food_position)
+print(score)
