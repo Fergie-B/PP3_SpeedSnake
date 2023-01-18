87cbf33651892f9ce78ef393e34a6b5957169f1b
Barry Ferguson <barry@revolvecreative.ie>
2023-01-18 20:29:21 +0000

    Moved code to Run.py to simplify process

--- run.py
+++ run.py
@@ -14,10 +14,40 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('speed_snake_scores')
 
-easy = SHEET.worksheet('easy')
-medium = SHEET.worksheet('medium')
-hard = SHEET.worksheet('hard')
+scorelog = SHEET.worksheet('scorelog')
 
-data = easy.get_all_values()
+data = scorelog.get_all_values()
 
 print(data)
+
+# import random module
+import random
+
+# import curses module
+import curses
+
+import time
+
+
+# set up screen
+setup = curses.initscr()
+h, w = setup.getmaxyx()
+win = curses.newwin(h, w, 0, 0)
+
+win.keypad(1)
+curses.curs_set(0)
+
+
+# Set Snake and Food Start positions
+snake_head = [10,10]
+food_position = [15,15]
+score = 0 
+
+# Show the Food on screen
+win.addch(food_position[0], food_position[1], curses.ACS_BULLET)
+print(food_position)
+
+prev_button_direction = 1
+button_direction = 1
+key = curses.KEY_RIGHT
+
